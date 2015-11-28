a5a8d1f8709c4171d1df905e45dc4bc9b87f8cf7
StaNov <stanovec@gmail.com>
2015-11-28 20:14:39 +0100

    fix speed after collision

--- Pong/Assets/Game/scripts/BallSpeedUpOnCollision.cs
+++ Pong/Assets/Game/scripts/BallSpeedUpOnCollision.cs
@@ -5,7 +5,19 @@ public class BallSpeedUpOnCollision : MonoBehaviour {
 
     public float velocityMultiplierOnHit = 1.1f;
 
+	private static float SQRT_2 = Mathf.Sqrt(2);
+
     void OnCollisionExit(Collision collision) {
-        collision.gameObject.GetComponent<Rigidbody>().velocity *= velocityMultiplierOnHit;
+		FixDirection(collision);
     }
+
+	void FixDirection(Collision collision) {
+		Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+		float magnitude = rb.velocity.magnitude;
+
+		rb.velocity = new Vector3(Mathf.Sign(rb.velocity.x), Mathf.Sign(rb.velocity.y), 0f);
+		rb.velocity *= magnitude / (SQRT_2);
+
+		rb.velocity *= velocityMultiplierOnHit;
+	}
 }

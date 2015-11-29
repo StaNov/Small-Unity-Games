af8d8d9c242ceb9d503086e98e8e5da65d904c39
StaNov <stanovec@gmail.com>
2015-11-29 19:23:25 +0100

    refactoring + platform dependent compilation

---
+++ Pong/Assets/Menu/ExitButtonHider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitButtonHider : MonoBehaviour {
+	
+#if (UNITY_WEBGL || UNITY_WEBPLAYER) && !UNITY_EDITOR
+	void Start () {
+		Destroy(GameObject.Find("Exit"));
+		Destroy(GameObject.Find("space exit"));
+	}
+#endif
+}
